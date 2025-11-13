7c82cbf1f44c88ec95c3fd53b40223da1cce0b36
ygstudio-game <yadnyeshsunilborole@gmail.com>
2025-11-14 00:01:04 +0530

    added the oauth callback url

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: callbackURL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
